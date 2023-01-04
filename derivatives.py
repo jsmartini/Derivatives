70debfcd88859dac888c03535583b1d05c1e4775
jsmartini <jsmartini@crimson.ua.edu>
2023-01-03 18:47:15 -0600

    working version

--- derivatives.py
+++ derivatives.py
@@ -6,7 +6,8 @@ from matplotlib import pyplot as plt
 from typing import *
 from dataclasses import dataclass
 from datetime import datetime
-import timedelta
+import dateutil.relativedelta as tdelta
+from enum import Enum
 
 """
     Black Scholes Evaluation Functions
@@ -41,6 +42,38 @@ LForward = lambda S, K: S-K
 SForward = lambda S, K: K-S
 
 
+# enums to describe the trade details
+
+class OptionType(Enum):
+    Call = 0
+    Put = 1
+    Forward = 2
+
+class ExerciseType(Enum):
+    # use a wrapper function for each contract to return and execution flag in the future for monte carlo
+    asian = 0
+    european = 1
+    american = 2
+
+class OptionSide(Enum):
+    Short = 0
+    Long = 1
+
+class TimeFrequency(Enum):
+    Day = 0
+    Week = 1
+    Month = 2
+    Year = 3
+
+def time2float(td: tdelta, config: TimeFrequency):
+    total = td.total_seconds()
+    match config:
+        case TimeFrequency.Day: return total / tdelta(days=1)
+        case TimeFrequency.Week: return total / tdelta(weeks=1)
+        case TimeFrequency.Month: return total / tdelta(months=1)
+        case TimeFrequency.Year: return total / tdelta(years=1)
+
+
 """
     OptionStrategy:
     - Constructor:
@@ -49,13 +82,32 @@ SForward = lambda S, K: K-S
 """
 
 @dataclass
-class Leg:
-    partial: function           # function handle
-    strike:  float              # strike price for the underlying
-    expiry:  int                # expiration date in years
-    riskfree: float             # risk free rate   
-    dividend: float             # continuous dividend rate
-    volatility: float           # standard deviation 
+class Leg:                      # describes a subcontract of a strategy
+    Side : OptionSide
+    Type : OptionType
+    Partial: function           # function handle
+    Strike:  float              # strike price for the underlying
+    Expiry:  int                # expiration date in years
+    Riskfree: float             # risk free rate   
+    Dividend: float             # continuous dividend rate
+    Volatility: float           # standard deviation 
+    Spot: float
+    Tfreq: TimeFrequency 
+    Quantity: int
+
+def valuationFunction(duration: float, contract: Leg):
+    # extendable valutation function
+    bs_args = [contract.Spot, contract.Strike, duration, contract.Riskfree, contract.Dividend, contract.Volatility]
+    match contract.Type:
+        case OptionType.Call:
+            # black scholes
+            return bs_call(*bs_args)
+        case OptionType.Put:
+            # black scholes
+            return bs_put(*bs_args)
+        case OptionType.Forward:
+            # fair value for forward contracts
+            return contract.Spot / np.exp(duration)
 
 class OptionStrategy(object):
 
@@ -63,16 +115,45 @@ class OptionStrategy(object):
         self.current_date = current_date
         self.Spots = np.linspace(0,spot_max, 100*spot_max)
         self.Derivatives = Derivatives
-        self.Parts  = List[Tuple[Leg, np.array]]    # contains sum of parts
+        self.Parts  = List[Dict]
         self.payoff = np.zeros(self.Spots.shape)
+        self.valuation = 0
+    def eval(self):
+        self.Parts.clear()          # clear old evaluations
+        for derivative in self.Derivatives:     
+            time = time2float(tdelta(self.current_date, derivative.Expiry), derivative.Tfreq)   # convert to time float
+            # easy unpack in the "bsvalue" key
+            self.Parts.append(
+                {
+                "Leg": derivative,
+                "payoff": derivative.function(self.Spots),
+                "bsvalue": valuationFunction(time, derivative)
+                }
+            )
+        self.payoff = np.sum([component["payoff"]*component["Quantity"] for component in self.Parts])
+        self.valuation = np.sum([component["bsvalue"]*component["Leg"].Quantity*(-1 if component["Leg"].OptionSide == OptionSide.Long else 1) \
+             for component in self.Parts])
 
-    def payoff(self):
-        for derivative in self.Derivatives:     self.Parts.append(tuple(derivative, derivative.function(self.Spots)))
-        self.payoff = np.sum([payoff for (_, payoff) in self.Parts])
+    def changeFieldAndReEval(self, field, value):
+        for idx, obj in enumerate(self.Derivatives):
+            try:
+                setattr(obj, field, value)
+            except BaseException as e:
+                print(e)
+                continue
+            self.Derivatives[idx] = obj
+        self.eval()
     
-    def bsvalue(self):
-        
-        
+    def reportData(self):
+        return {
+            "parts": self.Parts,
+            "payoff": self.payoff,
+            "valuations": self.valuation
+        }
+
+    
+
+
         
     
     
